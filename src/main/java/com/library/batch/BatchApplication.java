f807e2e4f430bd9626b0aa23d03ea827bf1171b2
Coubiac <benoit.grisot@gmail.com>
2020-08-01 23:53:14 +0200

    correction pom

--- src/main/java/com/library/batch/BatchApplication.java
+++ src/main/java/com/library/batch/BatchApplication.java
@@ -2,14 +2,10 @@ package com.library.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableFeignClients("com.library")
-@EnableDiscoveryClient
 public class BatchApplication {
 
 	public static void main(String[] args) {
