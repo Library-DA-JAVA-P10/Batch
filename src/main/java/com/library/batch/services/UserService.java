18bec02a281f5c5dbc2742f8c15c2263627bc5b9
Coubiac <benoit.grisot@gmail.com>
2020-08-03 23:14:12 +0200

    correction bug propriétés

--- src/main/java/com/library/batch/services/UserService.java
+++ src/main/java/com/library/batch/services/UserService.java
@@ -2,6 +2,7 @@ package com.library.batch.services;
 
 
 import com.library.batch.beans.UserBean;
+import com.library.batch.configuration.ApplicationConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -19,11 +20,14 @@ public class UserService {
     @Autowired
     MyRequestFactory myRequestFactory;
 
+    @Autowired
+    ApplicationConfiguration applicationConfiguration;
+
     public UserBean getUserById(String userId){
 
         RestTemplate restTemplate = myRequestFactory.getRestTemplate();
         HttpEntity<String> request = new HttpEntity<>("body", securityService.authenticate());
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost:9004/users/users/" + userId);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(applicationConfiguration.getApiUserBaseUrl() + userId);
         ResponseEntity<UserBean> response = restTemplate.exchange(builder.build().encode().toUri(),
                 HttpMethod.GET,
                 request,
